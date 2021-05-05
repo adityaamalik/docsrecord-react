23c2e0c6a1db79fa317c6a3f5860c5842a9fa888
Aditya Malik <aditya007@outlook.com>
2021-05-05 16:46:02 +0530

    backend url correct

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ axios.defaults.headers.common[`Authorization`] = `Bearer ${localStorage.getItem(
   "token"
 )}`;
 axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://docsrecord-backend.herokuapp.com";
 const App = () => (
   <BrowserRouter>
     <Router />
